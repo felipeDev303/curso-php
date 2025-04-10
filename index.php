6c4d7dc0ccec959cca644a1981a7827418b517d2
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-04-09 22:17:07 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// una variable
+$name = "Felipe";
+
+?>
+
+<h1>
+<?= "¡Mi primera app!"; ?>
+</h1>
+
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+</style>
