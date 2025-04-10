c11db009a5a8dde97daaae929dea0ba8ea733d45
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-04-10 02:11:41 -0400

    Updated index.php

---
+++ variable/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// echo
+
+echo "hola mundo";
+
+// Las variables comienzan con $, luego letra o _. Números solo después de estos.
+$a = 1;
+$A = 2;
+
+echo $a." ";
+echo $a + $A;
+
+?>
+
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+</style>
