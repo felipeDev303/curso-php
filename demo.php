69fb807a5b0af824d2ffde8b12aa43ee6d739209
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-04-12 01:10:15 -0400

    Updated files

--- demo.php
+++ demo.php
@@ -1,1 +1,88 @@
 <?php
+$name = "Felipe";
+//$name = "PHP";
+$isDev = true;
+$age = 36;
+$newAge = (bool) $age + '1';
+
+$isOld = $age > 40;
+
+// Ternaria
+$outputAge = $isOld
+    ? 'Eres viejo'
+    : 'Eres joven';
+
+// Match
+$outputAge2 = match (true) {
+    $age <3 => "Bebe, $name",
+    $age <11 => "Niño, $name",
+    $age < 18 => "Adolescente, $name",
+    $age === 18 => "Mayor de edad, $name",
+    $age <40 => "Adulto joven, $name",
+    $age < 60 => "Adulto viejo, $name",
+    default => "Viejo, $name",
+
+};
+
+$bestLanguages = ["PHP", "JavaScript", "Python",1,"C"];
+$bestLanguages [3] = "Java";
+$bestLanguages [] = "TypeScript";
+//var_dump($isDev);
+//var_dump($newAge);
+//var_dump($age);
+//var_dump($name);
+
+//echo gettype($name);
+//echo gettype($age);
+//echo gettype($newAge);
+//echo gettype($isDev);
+
+$output = "Hola $name, con una edad de $age.";
+//$output2 = 'Output 2: el $name no ve ya que no se interpolan las cadenas.';
+//$output2 .= "Output 3: o $age";
+//$output3 = "Output 3: saltando el carácter $ en: \$name";
+
+
+define('LOGO_URL', 'https://cdn.freebiesupply.com/logos/large/2x/php-1-logo-svg-vector.svg');
+
+$person = [
+    "name" => "Felipe",
+    "age" => 36,
+    "languages" => ["PHP", "JavaScript", "Python", "C", "TypeScript"],
+    "isDev" => true,
+];
+$person["languages"][] = "Java";
+$person["name"][] = "Ronaldinho";
+?>
+
+<ul>
+    <?php foreach ($bestLanguages as $key => $language): ?>
+        <li><?= $key . " " . $language ?></li>
+    <?php endforeach; ?>
+</ul>
+
+<img src="<?= LOGO_URL ?>" alt="PHP Logo" width="200">
+
+
+<h1>
+    <?= $output ?>
+</h1>
+
+<h2>
+    <?= $outputAge2 ?>
+</h2>
+
+<h3>
+    El mejor lenguaje es <?= $bestLanguages[0] ?>
+</h3>
+
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+</style>
--- index.php
+++ index.php
@@ -1,12 +1,52 @@
 <?php
-// una variable
-$name = "Felipe";
 
+const API_URL = "https://whenisthenextmcufilm.com/api";
+# Inicializar una nueva sesión de cURL; ch = cURL handle
+$ch = curl_init(API_URL);
+
+
+// Indicar que queremos recibir el resultado de la petición, no mostrarlo en pantalla
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+/*
+Ejecutar la petición
+y guardamos el resultado
+*/
+$result = curl_exec($ch);
+
+// Una alternativa sería utilizar file_get_contents
+// $result = file_get_contents(API_URL); // Si solo quires un GET de una API
+
+$data = json_decode($result, true);
+
+curl_close($ch);
 ?>
 
-<h1>
-<?= "¡Mi primera app!"; ?>
-</h1>
+<head>
+    <meta charset="UTF-8"/>
+    <title>La próxima película de Marvel</title>
+    <meta name="description" content="La próxima película de Marvel" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+    />
+</head>
+
+<main>
+
+    <section>
+        <img src="<?= $data["poster_url"]; ?>" width="200" alt="Poster de <?= $data["title"]; ?>"
+        style="border-radius: 16px"/>
+    </section>
+
+    <hgroup>
+        <h3><?= $data["title"]; ?> se estrena en <?= $data["days_until"]; ?> días</h3>
+        <p>Fecha de estreno: <?= $data["release_date"]; ?></p>
+        <p>La siguiente es: <?= $data["following_production"]["title"]; ?></p>
+    </hgroup>
+
+</main>
 
 <style>
     :root {
@@ -16,5 +56,22 @@ $name = "Felipe";
     body {
         display: grid;
         place-content: center;
+    }
+
+    section {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+    }
+
+    hgroup {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
+
+    img {
+        margin: 0 auto;
     }
 </style>
